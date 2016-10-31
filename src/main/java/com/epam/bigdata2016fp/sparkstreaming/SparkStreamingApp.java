7fb5b2f9a709e576c6703185a73b692317e172f5
Ilya_Starushchanka <ilya10194@mail.ru>
2016-10-31 17:11:38 +0300

    small fixes

--- src/main/java/com/epam/bigdata2016fp/sparkstreaming/SparkStreamingApp.java
+++ src/main/java/com/epam/bigdata2016fp/sparkstreaming/SparkStreamingApp.java
@@ -63,7 +63,7 @@ public class SparkStreamingApp {
             ESModel model = ESModel.parseLine(keyValue._2());
             CityInfo cityInfo = brCitiesDict.value().get(Integer.toString(model.getCity()));
             model.setGeoPoint(cityInfo);
-            model.setMlResult(model1.predict(Vectors.dense(model.getOsName().hashCode(),
+            Double mlResult= model1.predict(Vectors.dense(model.getOsName().hashCode(),
                     model.getDevice().hashCode(),
                     model.getUaFamily().hashCode(),
                     model.getRegion(),
@@ -73,7 +73,8 @@ public class SparkStreamingApp {
                     model.getAddSlotHeight(),
                     model.getAddSlotVisability(),
                     model.getAddSlotFormat(),
-                    brTagsDict.value().get(model.getUserTags()).split(",")[0].hashCode())));
+                    brTagsDict.value().get(model.getUserTags()).split(",")[0].hashCode()));
+            model.setMlResult(mlResult.intValue());
             //double result2 = tree.predict(Vectors.dense(0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0));
             return model;
         }).map(ESModel::toStringifyJson).foreachRDD(jsonRdd -> JavaEsSpark.saveJsonToEs(jsonRdd, confStr));
